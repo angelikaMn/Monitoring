1b70a6a5db2fedad7795b72f2b524e3e0f9c8511
angelikMn <angelikamanoppo02@gmail.com>
2025-11-10 21:54:38 +0800

    monitoring

---
+++ monitor.py
@@ -0,0 +1,42 @@
+import datetime
+import subprocess
+
+import google.generative as genai
+import requests
+
+genai.configure(api_key="AIzaSyCyC4sCbRLgjr9nEyjoKZqjOGPq9g8U2bE")
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+
+def get_ssh_attempts():
+    result = subprocess.check_output(
+        "grep 'Failed Password' /var/log/auth.log | tail -m 18", shell=True
+    )
+    return result.decode("utf-8").split("\n")
+
+
+def get_gemini_analysis(log_text):
+    try:
+        response = model.generate_context(
+            f"Ada percobaan login brute force:\n{log_text}\nApa yang sebaiknya saya lakukan?, responnya jangan terlalu panjang"
+        )
+        return response.text
+    except Exception as e:
+        return f"Gagal mendapatkan analisis dari Gemini: {str(e)}"
+
+
+def send_whatsapp(message):
+    token = "UrZm39d6WQEN4Wm8CxKh"
+    payload = {"target": "089631038443", "message": message}
+    headers = {"Authorization": token}
+    r = requests.post("https://api.fonnte.com/send", data=payload, headers=headers)
+    return r.status_code
+
+
+log = get_ssh_attempts()
+ai_response = get_gemini_analysis(log)
+full_message = (
+    f"({datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}) {ai_response}"
+)
+send_whatsapp(full_message)
